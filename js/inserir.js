0d9e177eb822f12bcbfb142b589d7013cb77b0fa
Guilherme Henrique <100978223+ghps2704@users.noreply.github.com>
2024-04-04 13:19:23 -0300

    Update inserir.js

--- js/inserir.js
+++ js/inserir.js
@@ -10,7 +10,7 @@ app.use(cors());
 
 // Configurações da conexão
 const client = new Client({
-	user: "postgres",
+	user: "ghps2704",
 	host: "localhost",
 	database: "postgres",
 	password: "pgadmin",
